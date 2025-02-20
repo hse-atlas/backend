b19999f5470643120bd5f1ea60af4a7875f66ece
Чеченов Роберт Адамович <rachechenov@edu.hse.ru>
2025-02-20 17:55:10 +0300

    total changes

--- app/main.py
+++ app/main.py
@@ -6,19 +6,19 @@ from app.project_CRUD import router as project_crud_router
 from app.user_CRUD import router as user_crud_router
 from app.user_auth import router as user_auth_router
 
-app = FastAPI(title="API Gateway", debug=True)
+application = FastAPI(title="API Gateway", debug=True)
 
-app.include_router(admin_auth_router, prefix="/api/v1/AuthService")
-app.include_router(user_auth_router, prefix="/api/v1/AuthService")
-app.include_router(project_crud_router, prefix="/projects")
-app.include_router(user_crud_router, prefix="/users")
+application.include_router(admin_auth_router, prefix="/api/v1/AuthService")
+application.include_router(user_auth_router, prefix="/api/v1/AuthService")
+application.include_router(project_crud_router, prefix="/projects")
+application.include_router(user_crud_router, prefix="/users")
 
 
-@app.get("/")
+@application.get("/")
 async def root():
     return {"message": "API Gateway is working"}
 
 
-@app.on_event("startup")
+@application.on_event("startup")
 async def startup_event():
     await test_db_connection()
